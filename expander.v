d77298065f2d1e6cffb563300f5a590a8168b4ef
j4ckyl06 <45118038+j4ckyl06@users.noreply.github.com>
2019-05-01 14:11:27 -0700

    Add files via upload

--- expander.v
+++ expander.v
@@ -39,6 +39,67 @@
         assign Y = {X[16:0], X[31:17]} ^ {X[18:0], X[31:18]} ^ {10'b0000000000, X[31:10]};
         
     endmodule
+    
+    module demux(
+        input [511:0] X,
+        input [3:0] sel,
+        output reg [31:0] Y
+        );
+        
+        always@(*) begin
+            case(sel)
+                4'd0: begin
+                    Y = X[31:0];
+                end
+                4'd1: begin
+                    Y = X[63:32];
+                end
+                4'd2: begin
+                    Y = X[95:64];
+                end
+                4'd3: begin
+                    Y = X[127:96];
+                end
+                4'd4: begin
+                    Y = X[159:128];
+                end
+                4'd5: begin
+                    Y = X[191:160];
+                end
+                4'd6: begin
+                    Y = X[223:192];
+                end
+                4'd7: begin
+                    Y = X[255:224];
+                end
+                4'd8: begin
+                    Y = X[287:256];
+                end
+                4'd9: begin
+                    Y = X[63:32];
+                end
+                4'd10: begin
+                    Y = X[63:32];
+                end
+                4'd11: begin
+                    Y = X[63:32];
+                end
+                4'd12: begin
+                    Y = X[63:32];
+                end
+                4'd13: begin
+                    Y = X[63:32];
+                end
+                4'd14: begin
+                    Y = X[63:32];
+                end
+                4'd15: begin
+                    Y = X[511:32];
+                end
+                default: Y = 0;
+            endcase    
+        end
+    endmodule
 //////////////////////////////////////////////////////////////////////////////////
 
 module expander(
@@ -47,10 +108,124 @@ module expander(
 //    input [511:0] M,             //final output of padding that gives out N amount of 512 bits block
 //    output [2044:0] W,
     //if reading in parsed msgs already of a 512 bits block broken into 16 Mt blocks of 32 bits
-    input [511:0] data_in,
+    input [511:0] dataIn,
     
-    output [31:0] Wt0, Wt1, Wt2, Wt3, Wt4, Wt5, Wt6, Wt7, Wt8, Wt9, Wt10, Wt11, Wt12, Wt13, Wt14, Wt15, W16, W17, W18, W19, W20, W21, W22, W23, W24, W26, W27, W28, W29, W30, W31, W32, W33, W34, W35, W36, W37, W38, W39, W40, W41, W42, W43, W44, W45, W46, W47, W48, W49, W50, W51, W52, W53, W54, W55, W56, W57, W58, W59, W60, W61, W62,
+    output [31:0] Wt0, Wt1, Wt2, Wt3, Wt4, Wt5, Wt6, Wt7, Wt8, Wt9, Wt10, Wt11, Wt12, Wt13, Wt14, Wt15, W16, W17, W18, W19, W20, W21, W22, W23, W24, W26, W27, W28, W29, W30, W31, W32, W33, W34, W35, W36, W37, W38, W39, W40, W41, W42, W43, W44, W45, W46, W47, W48, W49, W50, W51, W52, W53, W54, W55, W56, W57, W58, W59, W60, W61, W62, W63
     );
+    
+    reg [31:0] Mt0;
+    reg [31:0] Mt1;
+    reg [31:0] Mt2;
+    reg [31:0] Mt3;
+    reg [31:0] Mt4;
+    reg [31:0] Mt5;
+    reg [31:0] Mt6;    
+    reg [31:0] Mt7;
+    reg [31:0] Mt8;
+    reg [31:0] Mt9;
+    reg [31:0] Mt10;
+    reg [31:0] Mt11;
+    reg [31:0] Mt12;
+    reg [31:0] Mt13;
+    reg [31:0] Mt14;
+    reg [31:0] Mt15;
+    
+    demux DM0(     
+        .X(dataIn),
+        .sel(4'd0),
+        .Y(Mt0)    
+        );
+        
+    demux DM1(     
+        .X(dataIn),
+        .sel(4'd1),
+        .Y(Mt0)    
+        );   
+        
+    demux DM2(     
+        .X(dataIn),
+        .sel(4'd2),
+        .Y(Mt0)    
+        );         
+    
+    demux DM3(     
+        .X(dataIn),
+        .sel(4'd3),
+        .Y(Mt0)    
+        );   
+    
+    demux DM4(     
+        .X(dataIn),
+        .sel(4'd4),
+        .Y(Mt0)    
+        );   
+    
+    demux DM5(     
+        .X(dataIn),
+        .sel(4'd5),
+        .Y(Mt0)    
+        );
+    
+    demux DM6(     
+        .X(dataIn),
+        .sel(4'd6),
+        .Y(Mt0)    
+        );   
+        
+    demux DM7(     
+        .X(dataIn),
+        .sel(4'd7),
+        .Y(Mt0)    
+        ); 
+    demux DM8(     
+        .X(dataIn),
+        .sel(4'd8),
+        .Y(Mt0)    
+        );   
+        
+    demux DM9(     
+        .X(dataIn),
+        .sel(4'd9),
+        .Y(Mt0)    
+        );  
+           
+    demux DM10(     
+        .X(dataIn),
+        .sel(4'd10),
+        .Y(Mt0)    
+        );
+    
+    demux DM11(     
+        .X(dataIn),
+        .sel(4'd11),
+        .Y(Mt0)    
+        );
+    
+    demux DM12(     
+        .X(dataIn),
+        .sel(4'd12),
+        .Y(Mt0)    
+        );
+    
+    demux DM13(     
+        .X(dataIn),
+        .sel(4'd13),
+        .Y(Mt0)    
+        );
+    
+    demux DM14(     
+        .X(dataIn),
+        .sel(4'd14),
+        .Y(Mt0)    
+        );
+    
+    demux DM15(     
+        .X(dataIn),
+        .sel(4'd15),
+        .Y(Mt0)    
+        );
+    
+    reg [31:0] sigmaL1Out, sigmaL0Out;
     
     assign Wt0 = Mt0;
     assign Wt1 = Mt1;
@@ -68,7 +243,9 @@ module expander(
     assign Wt13 = Mt13;
     assign Wt14 = Mt14;
     assign Wt15 = Mt15;
-    assign Wt16 = {sigmaL1 s16(.X(Mt14), .Y(Wt16))};
+    
+    sigmaL1 s16(.X(Mt14), .Y(sigmaL1Out));
+    assign Wt16 = {(sigmaL1Out), W};
     assign Wt17 = {};
     assign Wt18 = ;
     assign Wt19 = ;
